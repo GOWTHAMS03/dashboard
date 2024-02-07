d8b3c8cb98d2309933f8ddc714ecbf3692991d6b
GOWTHAMS03 <gowthammani00007@gmail.com>
2024-02-07 14:36:18 +0530

    create projects

---
+++ dashboard-spring/src/main/java/com/gowtham/dashboardspring/DashboardSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gowtham.dashboardspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardSpringApplication.class, args);
+	}
+
+}
